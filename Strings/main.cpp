c9cc02952865faab145893811cbac6dc54cd9125
Nikolay Elshin <nikolay19830223@gmail.com>
2023-06-04 21:18:09 +0800

    is_int_number, to_int_number

--- Strings/main.cpp
+++ Strings/main.cpp
@@ -9,6 +9,8 @@ char To_Lower(char str[]);
 char To_Upper(char str[]);
 char Shrink(char str[], int &size);
 bool Is_Palindrome(char str[]);
+bool Is_Int_Number(char str[]);
+int To_Int_Number(char str[]);
 
 void main()
 {
@@ -46,12 +48,18 @@ void main()
 	if (Is_Palindrome(str)) cout << "Строка является полиндромом" << endl << endl;
 	else cout << "Строка не полиндром" << endl << endl;
 
+	cout << "Определяем является ли строка целым числом" << endl;
+	if (Is_Int_Number(str)) cout << "Строка является целым числом" << endl << endl;
+	else cout << "Строка не целое число" << endl << endl;
 	
+	cout << "Если строка является целым числом, то возвращаем его значение" << endl;
+	if (Is_Int_Number(str)) cout << "Возвращаемое число - " << To_Int_Number(str) << ", показываем тип числа - " << typeid(To_Int_Number(str)).name() << endl << endl;
+	else cout << "Строка не целое число и мы не можем вывести его значение в int" << endl << endl;
 
 
-	cout << int(str[7]);
-	cout << str[size + 1];
-	cout << size % 2;
+	//cout << int(str[0]);
+	//cout << str[size + 1];
+	//cout << size % 2;
 
 }
 
@@ -111,4 +119,31 @@ bool Is_Palindrome(char str[])
 	if (size % 2 != 0) return false;
 	for (int i = 0, j = size - 1; i < size / 2; i++, j--) if (str[i] != str[j]) return false;
 	return true;
+}
+
+bool Is_Int_Number(char str[])
+{
+	int size = StringLenght(str);
+	for (int i = 0; i < size; i++) if ((int)str[i] < 48 || (int)str[i] > 57) return false;
+	return true;
+}
+
+int To_Int_Number(char str[])
+{
+	int size = StringLenght(str);
+	int number = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if ((int)str[i] == 48) number *= 10;
+		if ((int)str[i] == 49) number = number * 10 + 1;
+		if ((int)str[i] == 50) number = number * 10 + 2;
+		if ((int)str[i] == 51) number = number * 10 + 3;
+		if ((int)str[i] == 52) number = number * 10 + 4;
+		if ((int)str[i] == 53) number = number * 10 + 5;
+		if ((int)str[i] == 54) number = number * 10 + 6;
+		if ((int)str[i] == 55) number = number * 10 + 7;
+		if ((int)str[i] == 56) number = number * 10 + 8;
+		if ((int)str[i] == 57) number = number * 10 + 9;
+	}
+	return number;
 }
