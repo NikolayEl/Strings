9cb2b3ed60969e78409246c2c225ba2c524654ec
Nikolay Elshin <nikolay19830223@gmail.com>
2023-06-04 20:28:54 +0800

    To_Lower, To_Upper, Shrink

--- Strings/main.cpp
+++ Strings/main.cpp
@@ -3,7 +3,11 @@
 
 using namespace std;
 
+
 int StringLenght(const char str[]);
+char To_Lower(char str[]);
+char To_Upper(char str[]);
+char Shrink(char str[], int &size);
 
 void main()
 {
@@ -18,14 +22,36 @@ void main()
 	const int SIZE = 256;
 	char str[SIZE] = {};
 	cout << "Enter string: "; 
-	//cin >> str;
 	SetConsoleCP(1251);
 	cin.getline(str, SIZE);
-	//SetConsoleOutputCP(866);
 	SetConsoleCP(866);
+	int size = StringLenght(str);
 	cout << str << endl;
 	cout << "Размер введенной строки - " << StringLenght(str) << endl;
+	//--------------------------------------------------------------------------------------
+	//cout << "Переводим всю строку в нижний регистр" << endl;
+	//str[size] = To_Lower(str);
+	//cout << str << endl << endl;
+
+	//cout << "Переводим всю строку в верхний регистр" << endl;
+	//str[size] = To_Upper(str);
+	//cout << str << endl << endl;
+
+	cout << "Удаляем из строки все лишние пробелы" << endl;
+	str[size] = Shrink(str, size);
+	cout << str << endl << endl;	
+
+
+
+
+	
+
+
+	cout << int(str[7]);
+	cout << str[size + 1];
+
 }
+
 
 int StringLenght(const char str[])
 {
@@ -36,4 +62,42 @@ int StringLenght(const char str[])
 	int i = 0;
 	for (; str[i]; i++);
 	return i;
+}
+
+char To_Upper(char str[])
+{
+	int size = StringLenght(str);
+	for (int i = 0; i < size; i++)
+	{
+		if (((int)str[i] >= 97 && (int)str[i] <= 122) || ((int)str[i] >= -32 && (int)str[i] <= -1)) str[i] -= 32; // Работает ENG/RUS в действующих кодировках
+	}
+	return str[size];
+}
+
+char To_Lower(char str[])
+{
+	int size = StringLenght(str);
+	for (int i = 0; i < size; i++)
+	{
+		if (((int)str[i] >= 65 && (int)str[i] <= 92) || ((int)str[i] >= -64 && (int)str[i] <= -33)) str[i] += 32;// Работает ENG/RUS в действующих кодировках
+	}
+	return str[size];
+}
+
+char Shrink(char str[], int& size) // Пробел  32 в таблице
+{
+	char buffer[256]{};
+	for (int i = 0; i < size; i++)buffer[i] = str[i];
+	bool temp = false;
+	int j = 0;
+	for (int i = 0; i < size; i++, j++)
+	{
+		if (temp) if ((int)buffer[i] == 32) j--;
+		str[j] = buffer[i];
+		if ((int)buffer[i] == 32) temp = true;
+		else temp = false;
+	}
+	for (int i = j; i <= size; i++) str[i] = {};
+	size = j;
+	return str[size];
 }
